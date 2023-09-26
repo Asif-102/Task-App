85b3e197405d56779de44b7867c8b6248c1deb01
Asif-102 <asif1zero2@gmail.com>
2023-09-27 03:41:25 +0600

    using mongoose and use data validation and sanitization

---
+++ src/db/mongoose.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+mongoose.connect('mongodb://localhost:27017/task-manager-api');
+
+const User = mongoose.model("User", {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid");
+            }
+        }
+    },  
+    age: {
+        type: Number,
+        required: true,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error("Age must be a positive number");
+            }
+        }
+    }
+});
+
+const me = new User({
+    name: " Andrew ",
+    email: " AnDrew@gmail.com  ",
+    age: 27
+});
+
+me.save().then(()=>{
+    console.log(me);
+}).catch((error)=>{
+    console.log("Error**", error);
+})
+
+// const Task = mongoose.model("Task", {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const task = new Task({
+//     description: "Learn the Mongoose library",
+//     completed: false
+// })
+
+// task.save().then(()=>{
+//     console.log(task);
+// }).catch((error)=>{
+//     console.log("Error**", error);
+// })

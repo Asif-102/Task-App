c8e8eee5ff0f7b584584712ccc4cf59614a06a2c
Asif-102 <asif1zero2@gmail.com>
2023-09-27 04:53:56 +0600

    organaize files

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,60 +1,6 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api');
-
-const User = mongoose.model("User", {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Email is invalid");
-            }
-        }
-    },  
-    age: {
-        type: Number,
-        required: true,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error("Age must be a positive number");
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value){
-            if(value.toLowerCase().includes("password")){
-                throw new Error("Password length must be 6 and it doesn't contains password");
-            }
-        }
-    }
-});
-
-const me = new User({
-    name: "mike",
-    email: "MIke@gmail.com  ",
-    age: 27,
-    password: "kalkias"
-});
-
-me.save().then(()=>{
-    console.log(me);
-}).catch((error)=>{
-    console.log("Error**", error);
-})
 
 const Task = mongoose.model("Task", {
     description: {
@@ -66,15 +12,4 @@ const Task = mongoose.model("Task", {
         type: Boolean,
         default: false
     }
-})
-
-const task = new Task({
-    description: "Learn the Mongoose library",
-    completed: false
-})
-
-task.save().then(()=>{
-    console.log(task);
-}).catch((error)=>{
-    console.log("Error**", error);
 })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.post("/users", (req, res)=>{
+    console.log(req.body);
+    res.send("testing!");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

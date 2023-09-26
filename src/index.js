57671aefccf0117ff1cc30ee8660c6425545697c
Asif-102 <asif1zero2@gmail.com>
2023-09-27 05:02:46 +0600

    make a post request using mongoose

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", (req, res)=>{
-    console.log(req.body);
-    res.send("testing!");
+app.post("/users", (req, res)=>{    
+    const user = new User(req.body);
+
+    user.save().then(()=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
 })
 
 app.listen(port, () => {

48b55c7ec8f3bb2daddd845f3118ece8fdf1102b
Asif-102 <asif1zero2@gmail.com>
2023-09-27 05:32:08 +0600

    find a user using url params

--- src/index.js
+++ src/index.js
@@ -8,26 +8,48 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", (req, res)=>{    
+app.post("/users", (req, res) => {
     const user = new User(req.body);
 
-    user.save().then(()=>{
+    user.save().then(() => {
         res.status(201).send(user);
-    }).catch((e)=>{
+    }).catch((e) => {
         res.status(400).send(e);
+    })
+})
+
+app.get("/users", (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+})
+
+app.get("/users/:id", (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }).catch((e)=>{
+        res.status(500).send();
     })
 })
 
 app.post("/tasks", (req, res) => {
     const task = new Task(req.body);
 
-    task.save().then(()=>{
+    task.save().then(() => {
         res.status(201).send(task);
-    }).catch((e)=>{
+    }).catch((e) => {
         res.status(400).send(e);
     })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 })

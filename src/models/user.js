7a4e751c2819126dd66245051268f39dfd7c4aa4
Asif-102 <asif1zero2@gmail.com>
2023-09-28 00:09:22 +0600

    using middleware

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,30 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         trim: true,
         lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error("Email is invalid");
             }
         }
-    },  
+    },
     age: {
         type: Number,
         required: true,
         default: 0,
-        validate(value){
-            if(value < 0){
+        validate(value) {
+            if (value < 0) {
                 throw new Error("Age must be a positive number");
             }
         }
@@ -33,12 +34,24 @@ const User = mongoose.model("User", {
         required: true,
         trim: true,
         minlength: 7,
-        validate(value){
-            if(value.toLowerCase().includes("password")){
+        validate(value) {
+            if (value.toLowerCase().includes("password")) {
                 throw new Error("Password length must be 6 and it doesn't contains password");
             }
         }
     }
 });
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+
+    if(user.isModified("password")){
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+})
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;

5ceb2e7aa6dd6335fb0ab37597ba798ac87408e6
Asif-102 <asif1zero2@gmail.com>
2023-09-28 01:45:55 +0600

    stop duplicate email use

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/task-manager-api');
+mongoose.connect('mongodb://localhost:27017/task-manager-api', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    autoIndex: true,
+});
 

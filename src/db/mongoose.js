ede9edcb12616da9159c8d52618af953cf952f10
Asif-102 <asif1zero2@gmail.com>
2023-09-28 02:09:47 +0600

    fix some error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/task-manager-api', {
+mongoose.connect('mongodb://localhost:27017/task-manager-api',{
     useUnifiedTopology: true,
     useNewUrlParser: true,
     autoIndex: true,

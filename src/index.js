14b138d7ff803649409e8a668bebf87e8f718626
Asif-102 <asif1zero2@gmail.com>
2023-10-08 09:22:46 +0600

    use cors middleware

--- src/index.js
+++ src/index.js
@@ -2,26 +2,16 @@ const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
+const corsMiddleware = require("./middleware/cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === "GET") {
-//         res.send("GET requests are disabled");
-//     } else {
-//         next();
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send("Site is currently down. Check back soon!");
-// })
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
+app.use(corsMiddleware);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
